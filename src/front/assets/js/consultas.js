b71aa754c30e3a3bcc45885c3572d08b06936bb2
CleberGomesJr <clebergomes2051@gmail.com>
2025-05-23 14:17:07 -0300

    Fix: removendo os métodos do JS das consultas

--- src/front/assets/js/consultas.js
+++ src/front/assets/js/consultas.js
@@ -5,18 +5,5 @@ document.getElementById('form-consulta').addEventListener('submit', function (ev
     const especialidade = document.getElementById('especialidade').value;
     const data = document.getElementById('data').value;
     const hora = document.getElementById('hora').value;
-
-    const hoje = new Date().toISOString().split('T')[0];
-    if (data < hoje) {
-        alert('A data da consulta não pode ser anterior à data atual.');
-        return;
-    }
-
-    if (hora < '08:00' || hora > '18:00') {
-        alert('Horário inválido. As consultas são permitidas entre 08:00 e 18:00.');
-        return;
-    }
-
-    alert(`Consulta agendada com sucesso para ${nome}, em ${data} às ${hora}, na especialidade ${especialidade}.`);
-    this.reset();
+    
 });
